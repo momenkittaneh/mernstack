41b5133fb6c73dd00c140ec1a250835cffe105cb
momenkittaneh <momen.kittaneh93@outlook.com>
2021-07-26 02:02:55 +0300

    assigments

---
+++ React/todolist/src/App.js
@@ -0,0 +1,16 @@
+import Inserttodo from "./components/Inserttodo";
+
+function App() {
+
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <Inserttodo/>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
