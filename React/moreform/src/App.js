6282f8187875f85caf37e33dba47b35a2e89a4b4
momenkittaneh <momen.kittaneh93@outlook.com>
2021-07-26 09:40:51 +0300

    assigment

--- React/moreform/src/App.js
+++ React/moreform/src/App.js
@@ -1,13 +1,13 @@
+import logo from './logo.svg';
 import './App.css';
 import UserForm from './components/UserForm';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <UserForm /> 
-      </header>
-    </div>
+    
+        <UserForm  />
+    
   );
 }
 
-export default App;
+export default UserForm;
--- React/moreform/src/components/UserForm.js
+++ React/moreform/src/components/UserForm.js
@@ -2,52 +2,107 @@ import React, { useState } from  'react';
     
     
 const UserForm = (props) => {
-    const [firstname, setfirstname] = useState("");
-    const [lastname, setlastname] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");  
-    const [cpassword, setcpassword] = useState("");
+    const [FirstName, setFirstName] = useState("");
+    const [FirstNameError, setFirstNameError] = useState("");
+    const [LastName, setLastName] = useState("");
+    const [LastNameError, setLastNameError] = useState("");
+    const [email, setEmail] = useState("");
+    const [EmailError, setEmailError] = useState("");
+    const [password, setPassword] = useState(""); 
+    const [PasswordError, setPasswordError] = useState("");
+    const [confirmpassword, setConfirmPassword] = useState("");
+    const [confirmpasswordError, setConfirmPasswordError] = useState("");
     
-    const createUser = (e) => {
-        e.preventDefault();
-        const newUser = { firstname,lastname, email, password,cpassword };
-        console.log("Welcome", newUser);
-    };
+
     
-    return(
-      <>
-      <form onSubmit={ createUser }>
-            <div>
-                <label>first name: </label> 
-                <input type="text" onChange={ (e) => setfirstname(e.target.value) } />
-            </div>
-            <div>
-                <label>last name: </label> 
-                <input type="text" onChange={ (e) => setlastname(e.target.value) } />
-            </div>
-            <div>
-                <label>Email Address: </label> 
-                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
-            </div>
-            <div>
-                <label>Password: </label>
-                <input type="text" onChange={ (e) => setPassword(e.target.value) } />
-            </div>
-            <div>
-                <label>confirm Password: </label>
-                <input type="text" onChange={ (e) => setcpassword(e.target.value) } />
-            </div>
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = { FirstName,LastName, email, password,confirmpassword };
+        console.log("Welcome", newUser);
+    };
 
-        </form>
-        <h1>First Name :{firstname}</h1>
-        <h1>Last Name :{lastname}</h1>
-        <h1>Email :{email}</h1>
-        <h1>password :{password}</h1>
-        <h1>Confirm Password :{cpassword}</h1>
+        const HandelFirstName = (e) => {
+            setFirstName(e.target.value);
+            if(e.target.value.length < 2) {
+                setFirstNameError("First Name Must be at Least 2 Characters!");
+            } else{
+                setFirstNameError ("");
+            }
+            
+        }
+        const HandelLastName = (e) => {
+            setLastName(e.target.value);
+            if(e.target.value.length < 2) {
+                setLastNameError("Last Name Must be at Least 2 Characters!");
+            } else{
+                setLastNameError ("");
+            } 
+            
+        }
+        const HandelEmail = (e) => {
+            setEmail(e.target.value);
+            if(e.target.value.length < 5) {
+                setEmailError("Email Must be at Least 5 Characters!");
+            } else{
+                setEmailError("");
+            }
+            
+        }
+        const HandelPassword = (e) => {
+            setPassword(e.target.value);
+            if(e.target.value.length < 8) {
+                setPasswordError("Password Must be at Least 8 Characters!");
+            } else{
+                setPasswordError("");
+            }
+            if(password!=confirmpassword){
+                setConfirmPasswordError ("Password Must Match");
+            }else{
+                setConfirmPasswordError("");
+            }
+        }
 
-        </>
+    
+    return(
+        <form onSubmit={ createUser }>
+            <table>
+                <tr>
+                    <td><label>First Name:</label></td>
+                    <td><input type="text" onChange={HandelFirstName} /></td>
+                    <td>{FirstNameError}</td>
+                </tr>
+                <tr>
+                    <td><label>Last Name: </label></td>
+                    <td><input type="text" onChange={ HandelLastName}/></td>
+                    <td>{LastNameError}</td>
+                </tr>
+                <tr>
+                    <td><label>Email : </label></td>
+                    <td><input type="text" onChange={HandelEmail} /></td>
+                    <td>{EmailError}</td>
+                </tr>
+                <tr>
+                    <td><label>Password: </label></td>
+                    <td><input type="text" onChange={ HandelPassword } /></td>
+                    <td>{PasswordError}</td>
+                </tr>
+                <tr>
+                    <td><label> Confirm Password: </label></td>
+                    <td><input type="text" onChange={ (e) => setConfirmPassword(e.target.value) } /></td>
+                    <td>{confirmpasswordError}</td>
+                </tr>
 
-    );
+                </table>
+            <h1> Your Form Data:</h1>
+                    <p>{FirstName}</p>
+                    <p>{LastName}</p>
+                    <p>{email}</p>
+                    <p>{password}</p>
+                    <p>{confirmpassword}</p>
+
+        </form>
+            
+    );
+    
 };
-    
-export default UserForm;
+export default UserForm
